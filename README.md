e82a03a73f5a3e43657c0728942681a9840c690d
kapoorkirpa07 <97598349+kapoorkirpa07@users.noreply.github.com>
2022-05-13 20:13:51 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pro.working
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ cannonBall.js
@@ -0,0 +1,55 @@
+class CannonBall {
+    constructor(x, y) {
+      var options = {
+        isStatic: true
+      };
+      this.r = 30;
+      this.body = Bodies.circle(x, y, this.r, options);
+      this.image = loadImage("bulletsimg.png");
+      this.trajectory = [];
+      World.add(world, this.body);
+    }
+  
+    remove(index) {
+      this.isSink = true;
+      Matter.Body.setVelocity(this.body, { x: 0, y: 0 });
+  
+      //this.animation = waterSplashAnimation;
+      this.speed = 0.05;
+      this.r = 150;
+      setTimeout(() => {
+        Matter.World.remove(world, this.body);
+        delete balls[index];
+      }, 1000);
+    }
+
+
+    shoot() {
+     var newAngle = cannon.angle - 28;
+      newAngle = newAngle *(3.14/180)
+      var velocity = p5.Vector.fromAngle(newAngle);
+      velocity.mult(0.5);
+      Matter.Body.setStatic(this.body, false);
+      Matter.Body.setVelocity(this.body, {
+        x: velocity.x *(-180/3.14), y: velocity.y * (-180/3.14)})
+    }
+  
+    display() {
+      var angle = this.body.angle;
+      var pos = this.body.position;
+      push();
+      imageMode(CENTER);
+      image(this.image, pos.x, pos.y, this.r, this.r);
+      pop();
+  
+      // Additional Activity
+      if (this.body.velocity.x > 0 && pos.x > -50) {
+        var position = [pos.x, pos.y];
+        this.trajectory.push(position);
+      }
+  
+      for (var i = 0; i < this.trajectory.length; i++) {
+        image(this.image, this.trajectory[i][0], this.trajectory[i][1], 5, 5);
+      }
+    }
+  }
